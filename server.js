0772a266cb31bd2f187eb367d9bb6bed6c547d2e
murgraydj <murgraydj@gmail.com>
2023-04-09 21:13:59 -0400

    Alert shows 'Hi!' when buttons are clicked

--- server.js
+++ server.js
@@ -4,17 +4,22 @@
 //     });
 //  });
 
-const http = require('http');
+const choiceBtns = document.getElementsByName('choice')
+choiceBtns.forEach(choice => choice.addEventListener('click', e => {
+  alert('Hi!')
+}))
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const http = require('https');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
